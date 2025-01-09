fbea7edca20714e80d05aa4547370a1ccf3c9096
gusfrancoo <gustrafranco2543@gmail.com>
2025-01-09 00:45:24 -0300

    correção de erros no build

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { PrincipalComponent } from './components/principal/principal.component';
-import { AlunosComponent } from './components/alunos/alunos.component';
+// import { AlunosComponent } from './components/alunos/alunos.component';
 
 export const routes: Routes = [
   {path: 'home', component: PrincipalComponent },
-  {path: 'alunos', component: AlunosComponent  }
+  // {path: 'alunos', component: AlunosComponent  }
 ];
